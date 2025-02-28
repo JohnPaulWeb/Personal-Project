76b63ff919859d4fc2a6eae1c204d9092caf1677
JohnPaulWeb <braganzajohnpaul@gmail.com>
2025-02-28 22:36:13 +0800

    Skills | Update

--- app/components/Experience.tsx
+++ app/components/Experience.tsx
@@ -1,12 +1,18 @@
+"use client"
+
+
+import { Briefcase, Calendar, Globe, MapPin } from "lucide-react"
 import AnimatedSection from "./AnimatedSection"
 
+import { motion } from "framer-motion"
+import Image from "next/image"
 
 export default function Experience() {
 
 
     const experience = [
         {
-            company: "The Rakuzan",
+            company: " Rakuzan",
             location: "Company",
             period: "2025 - Yakuza",
             role: "Person",
@@ -19,7 +25,7 @@ export default function Experience() {
         },
 
         {
-            company: "The Rakuzan",
+            company: " Rakuzan",
             location: "Company",
             period: "2025 - Yakuza",
             role: "Person",
@@ -32,7 +38,7 @@ export default function Experience() {
         },
 
         {
-            company: "The Rakuzan",
+            company: " Rakuzan",
             location: "Company",
             period: "2025 - Yakuza",
             role: "Person",
@@ -51,7 +57,55 @@ export default function Experience() {
          duration-300 overflow-hidden relative">
             
             <div className="container mx-auto px-6 relative z-10">
-                <AnimatedSection />
+                <AnimatedSection title="Ayanokoji Kiyotaka" />
+
+                <div className="space-y-16">
+                    {experience.map((exp, index) => (
+                        <motion.div key={index}
+                         initial={{ opacity: 0, y: 50}}
+                         whileInView={{ opacity: 1, y: 0 }}
+                         viewport={{ once: true }}
+                         transition={{ duration: 0.5, delay: index * 0.2 }}
+                         className="bg-white dark:bg-gray-800 p-8 rounded-xl shadow-lg
+                          transition-all duration-300 hover:shadow-2xl relative overflow-hidden group">
+
+                            <div className="absolute top-0 right-0 w-32 h-32 bg-blue-200 dark:bg-blue-700 rounded-bl-full z-0 opacity-50
+                             transition-transform duration-300 group-hover:scale-110">
+                            </div>
+
+                            <div className="relative z-10">
+                                <h3>{exp.company === "Companay" ? <Globe className="w-6 h-6 mr-2 text-blue-500" /> : null}{exp.company}</h3>
+                                <p className="text-gray-600 dark:text-gray-300 mb-4 flex items-center">
+                                    <MapPin className="w-4 h-4 mr-2" />
+                                    {exp.location}
+                                </p>
+
+                                <p className="text-gray-600 dark:text-gray-300 mb-4 flex items-center">
+                                    <Calendar className="w-4 h-4 mr-2" />
+                                    {exp.period}
+                                </p>
+
+                                <p className="text-gray-600 dark:text-gray-300 mb-4 flex items-center">
+                                    <Briefcase className="w-4 h-4 mr-2" />
+                                    {exp.role}
+                                </p>
+
+                                <ul className="list-none space-y-2">
+                                    {exp.resposibilities.map((resp, idx) => (
+                                        <li key={idx} className="text-gray-700 dark:text-gray-300 flex items-start">
+                                            <span className="text-blue-500 mr-2">.</span>
+                                            {resp}
+                                        </li>
+                                    ))}
+                                </ul>
+                            </div>
+                          </motion.div>
+                    ))}
+                    </div>
+            </div>
+
+            <div className="absolute bottom-0 right-0 w-64 h-64 -mb-32 -mr-32 opacity-20">
+                <Image src="/yas.jpg" alt="Ayanokoji Kiyotaka" width={256} height={256} />
             </div>
         </section>
     )
--- app/components/Skills.tsx
+++ app/components/Skills.tsx
@@ -1,9 +1,121 @@
+"use client"
 
+import { Code, Cpu, Database, GitBranch, Globe, Layers, Layout, Server, Terminal, Workflow } from "lucide-react";
+
+
+const SkillIcon = ({ icon: Icon, color }: {icon: any; color: string }) => (
+    <div className={`p-2 rounded-full bg-white dark:bg-gray-800 shadow-lg`}>
+        <Icon className={`w-6 h-6 ${color}`} />
+    </div>
+)
+
+
+const skills = [
+    {
+        icon: Code,
+        name: "Frontend Dev",
+        tech: "Reactjs",
+        description: "Build responsive website",
+        color: "text-blue-500",
+    },
+
+    {
+        icon: Server,
+        name: "Frontend Dev",
+        tech: "Reactjs",
+        description: "Build responsive website",
+        color: "text-blue-500",
+    },
+
+    {
+        icon: Database,
+        name: "Frontend Dev",
+        tech: "Reactjs",
+        description: "Build responsive website",
+        color: "text-blue-500",
+    },
+
+    {
+        icon: Layout,
+        name: "Frontend Dev",
+        tech: "Reactjs",
+        description: "Build responsive website",
+        color: "text-blue-500",
+    },
+
+    {
+        icon: GitBranch,
+        name: "Frontend Dev",
+        tech: "Reactjs",
+        description: "Build responsive website",
+        color: "text-blue-500",
+    },
+
+    {
+        icon: Terminal,
+        name: "Frontend Dev",
+        tech: "Reactjs",
+        description: "Build responsive website",
+        color: "text-blue-500",
+    },
+
+    {
+        icon: Layers,
+        name: "Frontend Dev",
+        tech: "Reactjs",
+        description: "Build responsive website",
+        color: "text-blue-500",
+    },
+
+    {
+        icon: Cpu,
+        name: "Frontend Dev",
+        tech: "Reactjs",
+        description: "Build responsive website",
+        color: "text-blue-500",
+    },
+
+    {
+        icon: Globe,
+        name: "Frontend Dev",
+        tech: "Reactjs",
+        description: "Build responsive website",
+        color: "text-blue-500",
+    },
+
+    {
+        icon: Workflow,
+        name: "Frontend Dev",
+        tech: "Reactjs",
+        description: "Build responsive website",
+        color: "text-blue-500",
+    },
+]
 
 export default function Skills() {
     return(
-        <div>
-            
+        <section id="skills" className="py-20 relative overflow-hidden">
+
+            <div className="absolute inset-0 bg-gradient-to-br from-blue-50 via-indigo-50 to-purple-50 dark:from-gray-900 dark:via-indigo-900 dark:to-purple-900">
+
+            </div>
+
+        <div className="absolute inset-0 opacity-10">
+            <svg className="w-full h-full" xmlns="">
+                <defs>
+                    <pattern id="skill-pattern" x="0" y="0" width="100" height="100" patternUnits="userSpaceOnUse">
+                        <circle cx="50" cy="50" r="20" fill="none" stroke="currenColor" strokeWidth="2" />
+
+                        <path d="M50 3- L50 m30 50 L70 50" stroke="currentColor" strokeWidth="2" />
+
+                    </pattern>
+                </defs>
+
+                <rect x="0" y="0" width="100%" height="1000%" fill="url (#skill-pattern)" />
+            </svg>
         </div>
+
+
+        </section>
     )
 }
